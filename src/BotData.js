eeef295a9d7a84ff334cee915b064ebb41e23d85
Samuel Radat <samuel.radat@epitech.eu>
2020-03-19 23:05:54 +0100

    [ADD] - botData

---
+++ src/BotData.js
@@ -0,0 +1,28 @@
+module.exports = {
+
+    BotValues: {
+        botId: '',
+        botOwners: [''],
+        botToken: '',
+        botPrefix: 'lg/',
+        botColor: [0, 210, 255],
+        botInviteLink: '',
+    },
+
+    GoogleSheet: {
+        apiKey: "",
+        spreadsheetId: ''
+    },
+
+    LG: {
+        running: false,
+        game: null,
+        canRun: [],
+        stemming: null
+    },
+
+    Settings: {
+        Admins: []
+    }
+
+};
